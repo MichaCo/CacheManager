b7b1d6ab39f85311b06c964ef9184d54a2a6f9dd
MichaCo <kletterer@web.de>
2015-04-01 12:50:00 +0200

    somd documentation update

--- src/CacheManager.Redis/RedisConfiguration.cs
+++ src/CacheManager.Redis/RedisConfiguration.cs
@@ -3,9 +3,26 @@ using System.Collections.Generic;
 
 namespace CacheManager.Redis
 {
+    /// <summary>
+    /// <see cref="RedisConfiguration"/> will be used for configuring e.g. StackExchange.Redis by code or configuration file.
+    /// <para>
+    /// The element was added only because StackExchange.Redis doesn't support configuration via web/app.config
+    /// </para>
+    /// </summary>
     [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Redis")]
     public sealed class RedisConfiguration
     {
+        /// <summary>
+        /// Creates a new <see cref="RedisConfiguration"/> element.
+        /// </summary>
+        /// <param name="key">The configuration key which will be used by the cache handle to find a configuration for the cache handle's name.</param>
+        /// <param name="endpoints">The list of <see cref="ServerEndPoint"/>s to be used to connect to Redis server.</param>
+        /// <param name="database">The Redis database index.</param>
+        /// <param name="password">The password of the Redis server.</param>
+        /// <param name="isSsl">If <c>true</c> instructs the cache to use SSL encryption.</param>
+        /// <param name="sslHost">If specified, the connection will set the ssl host.</param>
+        /// <param name="connectionTimeout">Sets the timeout used for connect operations.</param>
+        /// <param name="allowAdmin">If set to <c>True</c> it enables the cache to use features which might be risky. <c>Clear</c> for example.</param>
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1026:DefaultParametersShouldNotBeUsed")]
         public RedisConfiguration(
             string key, 
@@ -42,6 +59,11 @@ namespace CacheManager.Redis
             this.AllowAdmin = allowAdmin;
         }
 
+        /// <summary>
+        /// Creates a new <see cref="RedisConfiguration"/> element.
+        /// </summary>
+        /// <param name="key">The configuration key which will be used by the cache handle to find a configuration for the cache handle's name.</param>
+        /// <param name="connectionString">Instead of specifying all the properties, this can also be done via one connection string.</param>
         public RedisConfiguration(
             string key,
             string connectionString)
@@ -50,27 +72,54 @@ namespace CacheManager.Redis
             this.ConnectionString = connectionString;
         }
         /// <summary>
-        /// Gets the identifier for the redis options.
+        /// Gets the identifier for the redis configuration.
+        /// <para>
+        /// This might have to match with the cache handle's name to make the cache handle use this configuration.
+        /// </para>
         /// </summary>
         public string Key { get; private set; }
 
         /// <summary>
-        /// Gets the connection string. Will be used by some redis clients (Stackexchange.Redis).
+        /// Gets the connection string.
         /// </summary>
         public string ConnectionString { get; private set; }
 
+        /// <summary>
+        /// Gets the password to be used to connect to the Redis server.
+        /// </summary>
         public string Password { get; private set; }
 
+        /// <summary>
+        /// Instructs the Redis connection to use SSL encryption.
+        /// </summary>
         public bool IsSsl { get; private set; }
 
+        /// <summary>
+        /// If set, it will enforce this particular host on the server's certificate.
+        /// </summary>
         public string SslHost { get; private set; }
 
+        /// <summary>
+        /// Gets the timeout for any connect operations.
+        /// </summary>
         public int ConnectionTimeout { get; private set; }
 
+        /// <summary>
+        /// Gets the list of endpoints to be used to connect to the Redis server.
+        /// </summary>
         public IList<ServerEndPoint> Endpoints { get; private set; }
 
+        /// <summary>
+        /// Indicates if the connection is allowed to run certain 'risky' commands.
+        /// <para>
+        /// <c>cache.Clear</c> requires this to be set to true because we will flush the Redis database.
+        /// </para>
+        /// </summary>
         public bool AllowAdmin { get; private set; }
 
+        /// <summary>
+        /// Gets the Redis database index the cache will use.
+        /// </summary>
         public int Database { get; private set; }
     }
 
