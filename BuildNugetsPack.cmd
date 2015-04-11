08145640ecb28a6a975a92c2b846efd82b487c6a
MichaCo <kletterer@web.de>
2015-04-11 03:23:04 +0200

    update image

--- BuildNugetsPack.cmd
+++ BuildNugetsPack.cmd
@@ -1,11 +1,11 @@
 rem .nuget\NuGet Update -self
-.nuget\nuget.exe pack src\CacheManager.Core\CacheManager.Core.csproj -o D:\_Nuget -Prop Configuration=Release -verbosity detailed 
-.nuget\nuget.exe pack src\CacheManager.SystemRuntimeCaching\CacheManager.SystemRuntimeCaching.csproj -o D:\_Nuget -Prop Configuration=Release -verbosity detailed 
-.nuget\nuget.exe pack src\CacheManager.WindowsAzureCaching\CacheManager.WindowsAzureCaching.csproj -o D:\_Nuget -Prop Configuration=Release -verbosity detailed 
-.nuget\nuget.exe pack src\CacheManager.Web\CacheManager.Web.csproj -o D:\_Nuget -Prop Configuration=Release -verbosity detailed 
-.nuget\nuget.exe pack src\CacheManager.AppFabricCache\CacheManager.AppFabricCache.csproj -o D:\_Nuget -Prop Configuration=Release -verbosity detailed 
-.nuget\nuget.exe pack src\CacheManager.Memcached\CacheManager.Memcached.csproj -o D:\_Nuget -Prop Configuration=Release -verbosity detailed 
-.nuget\nuget.exe pack src\CacheManager.StackExchange.Redis\CacheManager.StackExchange.Redis.csproj -o D:\_Nuget -Prop Configuration=Release -verbosity detailed 
-.nuget\nuget.exe pack src\CacheManager.Couchbase\CacheManager.Couchbase.csproj -o D:\_Nuget -Prop Configuration=Release -verbosity detailed 
+.nuget\nuget.exe pack src\CacheManager.Core\CacheManager.Core.csproj -o D:\_Nuget\CacheManager -Prop Configuration=Release -verbosity detailed 
+.nuget\nuget.exe pack src\CacheManager.SystemRuntimeCaching\CacheManager.SystemRuntimeCaching.csproj -o D:\_Nuget\CacheManager -Prop Configuration=Release -verbosity detailed 
+.nuget\nuget.exe pack src\CacheManager.WindowsAzureCaching\CacheManager.WindowsAzureCaching.csproj -o D:\_Nuget\CacheManager -Prop Configuration=Release -verbosity detailed 
+.nuget\nuget.exe pack src\CacheManager.Web\CacheManager.Web.csproj -o D:\_Nuget\CacheManager -Prop Configuration=Release -verbosity detailed 
+.nuget\nuget.exe pack src\CacheManager.AppFabricCache\CacheManager.AppFabricCache.csproj -o D:\_Nuget\CacheManager -Prop Configuration=Release -verbosity detailed 
+.nuget\nuget.exe pack src\CacheManager.Memcached\CacheManager.Memcached.csproj -o D:\_Nuget\CacheManager -Prop Configuration=Release -verbosity detailed 
+.nuget\nuget.exe pack src\CacheManager.StackExchange.Redis\CacheManager.StackExchange.Redis.csproj -o D:\_Nuget\CacheManager -Prop Configuration=Release -verbosity detailed 
+.nuget\nuget.exe pack src\CacheManager.Couchbase\CacheManager.Couchbase.csproj -o D:\_Nuget\CacheManager -Prop Configuration=Release -verbosity detailed 
 
 @PAUSE
