982fa1f3bc424975e98c36bbd1d1229e305db00c
MichaCo <mconrad@email.de>
2015-06-05 18:46:47 +0200

    Removed .Redis project. Was merged into .Stackexchange.Redis already. Removed project references to AppFabricCache and WindowsAzureCaching (not building/testing those anymore).

--- src/CacheManager.Redis/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: Guid("b9f4b4e5-5ea8-40b2-9f07-a2c89eaf8aa3")]
-[assembly: AssemblyTitle("CacheManager.Redis")]
-[assembly: CLSCompliant(true)]
