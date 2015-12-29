365d0d307e1338f031ce8d9587a032fd84c3a780
MichaCo <kletterer@web.de>
2015-12-29 15:25:50 +0100

    Changing "HttpContext.Current is null" error to throw proper error message

--- src/CacheManager.Web/ContextFactory.cs
+++ src/CacheManager.Web/ContextFactory.cs
@@ -5,12 +5,12 @@ namespace CacheManager.Web
 {
     internal static class ContextFactory
     {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1305:SpecifyIFormatProvider", MessageId = "System.String.Format(System.String,System.Object)", Justification = "It's fine")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA2204:Literals should be spelled correctly", MessageId = "HttpContext", Justification = "External naming")]
         public static HttpContextBase CreateContext()
         {
             if (HttpContext.Current == null)
             {
-                throw new InvalidOperationException($"{nameof(HttpContext.Current)} is required for System.Web caching and must not be null.");
+                throw new InvalidOperationException("HttpContext.Current is required for System.Web caching and must not be null.");
             }
 
             return new HttpContextWrapper(HttpContext.Current);
