107ab3a9883e2e233626268aa444dee5b82c247d
AuroraDysis <auroradysis@gmail.com>
2016-07-24 21:57:21 +0800

    Add Microsoft.Extensions.Caching.Memory support and minimum test (#68)
    Add Microsoft.Extensions.Caching.Memory support and minimum test

---
+++ src/CacheManager.Microsoft.Extensions.Caching.Memory/MicrosoftMemoryCachingBuilderExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CacheManager.Core;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace CacheManager.Core
+{
+    /// <summary>
+    /// Extensions for the configuration builder specific to Microsoft.Extensions.Caching.Memory cache handle.
+    /// </summary>
+    public static class MicrosoftMemoryCachingBuilderExtensions
+    {
+        private const string DefaultName = "default";
+
+        public static ConfigurationBuilderCacheHandlePart WithMicrosoftMemoryCacheHandle(
+            this ConfigurationBuilderCachePart part, string instanceName)
+            => WithMicrosoftMemoryCacheHandle(part, instanceName, false);
+
+        public static ConfigurationBuilderCacheHandlePart WithMicrosoftMemoryCacheHandle(
+            this ConfigurationBuilderCachePart part)
+            => part?.WithHandle(typeof(MemoryCacheHandle<>), DefaultName, false);
+
+        public static ConfigurationBuilderCacheHandlePart WithMicrosoftMemoryCacheHandle(
+            this ConfigurationBuilderCachePart part, string instanceName, bool isBackplaneSource)
+            => part?.WithHandle(typeof(MemoryCacheHandle<>), instanceName, isBackplaneSource);
+    }
+}
