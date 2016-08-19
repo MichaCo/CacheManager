5cda0606fddece283df22ac11e6dcd44b1d4d761
Yash <yashpa@microsoft.com>
2016-08-19 11:30:22 -0700

    Adding Microsoft Bond Serialization Support to CacheManager

---
+++ src/CacheManager.Serialization.Bond/BondCacheItem.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using CacheManager.Core;
+using MSBond = Bond;
+
+namespace CacheManager.Serialization.Bond
+{
+    [MSBond.Schema]
+    public class BondCacheItem
+    {
+        [MSBond.Id(0)]
+        public DateTime CreatedUtc { get; set; }
+
+        [MSBond.Id(1)]
+        public DateTime LastAccessedUtc { get; set; }
+
+        [MSBond.Id(2)]
+        public ExpirationMode ExpirationMode { get; set; }
+
+        [MSBond.Id(3)]
+        public TimeSpan ExpirationTimeout { get; set; }
+
+        [MSBond.Id(4)]
+        public string Key { get; set; }
+
+        [MSBond.Id(5)]
+        public string Region { get; set; }
+
+        [MSBond.Id(6)]
+        public byte[] Value { get; set; }
+
+        public static BondCacheItem FromCacheItem<TValue>(CacheItem<TValue> item, byte[] value)
+        {
+            return new BondCacheItem
+            {
+                CreatedUtc = item.CreatedUtc,
+                LastAccessedUtc = item.LastAccessedUtc,
+                ExpirationMode = item.ExpirationMode,
+                ExpirationTimeout = item.ExpirationTimeout,
+                Key = item.Key,
+                Region = item.Region,
+                Value = value
+            };
+        }
+
+        public CacheItem<T> ToCacheItem<T>(object value)
+        {
+            var output = string.IsNullOrEmpty(Region) ?
+                    new CacheItem<T>(Key, (T)value, ExpirationMode, ExpirationTimeout) :
+                    new CacheItem<T>(Key, Region, (T)value, ExpirationMode, ExpirationTimeout);
+
+            output.LastAccessedUtc = LastAccessedUtc;
+            return output.WithCreated(CreatedUtc);
+        }
+    }
+}
---
+++ src/CacheManager.Serialization.Bond/BondSerializer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Bond.IO.Safe;
+using Bond.Protocols;
+using CacheManager.Core;
+using CacheManager.Core.Internal;
+using MSBond = Bond;
+using static CacheManager.Core.Utility.Guard;
+
+namespace CacheManager.Serialization.Bond
+{
+    public class BondSerializer : ICacheSerializer
+    {
+        public object Deserialize(byte[] data, Type target)
+        {
+            var input = new InputBuffer(data);
+            var reader = new CompactBinaryReader<InputBuffer>(input);
+            return new MSBond.Deserializer<CompactBinaryReader<InputBuffer>>(target).Deserialize(reader);
+        }
+
+        public CacheItem<T> DeserializeCacheItem<T>(byte[] value, Type valueType)
+        {
+            var bondItem = (BondCacheItem)this.Deserialize(value, typeof(BondCacheItem));
+            EnsureNotNull(bondItem, "Could not deserialize cache item");
+            var deserializedValue = this.Deserialize(bondItem.Value, valueType);
+            return bondItem.ToCacheItem<T>(deserializedValue);
+        }
+
+        public byte[] Serialize<T>(T value)
+        {
+            var output = new OutputBuffer();
+            var writer = new CompactBinaryWriter<OutputBuffer>(output);
+            MSBond.Serialize.To(writer, value);
+            return output.Data.Array;
+        }
+
+        public byte[] SerializeCacheItem<T>(CacheItem<T> value)
+        {
+            NotNull(value, nameof(value));
+            var jsonValue = this.Serialize(value.Value);
+            var jsonItem = BondCacheItem.FromCacheItem(value, jsonValue);
+            return this.Serialize(jsonItem);
+        }
+    }
+ 
+}
