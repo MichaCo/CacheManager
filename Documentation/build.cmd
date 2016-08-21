13949809d76cbf9f89023886e25527962aa6d1fd
MichaC <kletterer@web.de>
2016-08-21 16:02:48 +0200

    Fixing protobuf-net project.json deps to not link standard lib to full clr targets... Fix docfx build

--- Documentation/build.cmd
+++ Documentation/build.cmd
@@ -5,7 +5,11 @@ SETLOCAL
 SET NUGET_VERSION=latest
 SET CACHED_NUGET=%LocalAppData%\NuGet\nuget.%NUGET_VERSION%.exe
 
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "&{$Branch='dev';iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}"
+call dnvm install latest
+
 IF EXIST %CACHED_NUGET% goto copynuget
+
 echo Downloading latest version of NuGet.exe...
 IF NOT EXIST %LocalAppData%\NuGet md %LocalAppData%\NuGet
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest 'https://dist.nuget.org/win-x86-commandline/%NUGET_VERSION%/nuget.exe' -OutFile '%CACHED_NUGET%'"
@@ -21,6 +25,7 @@ IF EXIST msdn.4.5.2 goto build
 .nuget\nuget.exe install docfx -ExcludeVersion -pre -Out packages
 
 :build
+
 call dnu restore packages\docfx\app
 del "*.log"
 call packages\docfx\app\docfx.cmd metadata -l meta.log --logLevel info -f
