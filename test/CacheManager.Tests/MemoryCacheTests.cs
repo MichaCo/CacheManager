b69d1e7631a15cc9807eba7773f8dbc443d81d4a
MichaC <kletterer@web.de>
2017-06-15 14:38:30 +0200

    increase race condition test iterations

--- test/CacheManager.Tests/MemoryCacheTests.cs
+++ test/CacheManager.Tests/MemoryCacheTests.cs
@@ -245,7 +245,7 @@ namespace CacheManager.Tests
             };
 
             var exceptions = 0;
-            for (var i = 0; i < 10; i++)
+            for (var i = 0; i < 100; i++)
             {
                 Action act = () =>
                 {
